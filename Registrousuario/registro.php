2f983b381ba53bd93cfc536dcd806a33ff92411b
Keiichi243 <53916255+Keiichi243@users.noreply.github.com>
2020-05-24 01:30:03 -0500

    Update registro.php

--- Registrousuario/registro.php
+++ Registrousuario/registro.php
@@ -1,181 +1,30 @@
-<!doctype html>
-<html>
+<?php    
+    $Nombre = $_POST['nom'];
+	$Apellido = $_POST['ape'];
+	$Correo = $_POST['correo'];
+	$Telefono = $_POST['tel'];
+	$Direccion = $_POST['direc'];
+	$Genero = $_POST['genero'];
+	$FechaNacimiento = $_POST['fechanacim'];
+	$Ciudad = $_POST['ciudad'];
+	$Nacionalidad = $_POST['nacio'];
+    $pass = $_POST['pass'];
+    $rpass = $_POST['rpass'];
 
-<head>
-	<meta charset="utf-8">
-	<title>marketplace</title>
-	<!--llama la hoja de estilos-->
-	<link rel="stylesheet" type="text/css" href="../Css/estilo.css">
-</head>
-<style type="text/css">
-	.boton_login {
-		text-decoration: none;
-		padding: 5px;
-		font-weight: 600;
-		font-size: 20px;
-		color: #ffffff;
-		background-color: #000000;
-		border-radius: 6px;
-		border: 1px solid #0016b0;
-	}
-
-	.boton_login:hover {
-		color: #000000;
-		background-color: #ffffff;
-	}
-
-	.button {
-		text-decoration: none;
-		padding: 5px;
-		border-radius: 6px;
-		font-weight: 600;
-		font-size: 20px;
-		background-color: white;
-		color: black;
-		border: 2px solid #555555;
-	}
-
-	.button:hover {
-		background-color: #555555;
-		color: white;
-	}
-</style>
-
-<body>
-
-	<?php
-	include("../ConexionBase/Conexiondb.php");
-
-	if (isset($_POST["cr"])) {
-		$Nombre = $_POST['nom'];
-		$Apellido = $_POST['ape'];
-		$Correo = $_POST['correo'];
-		$Telefono = $_POST['tel'];
-		$Direccion = $_POST['direc'];
-		$Genero = $_POST['genero'];
-		$FechaNacimiento = $_POST['fechanacim'];
-		$Ciudad = $_POST['ciudad'];
-		$Nacionalidad = $_POST['nacio'];
-		$pass1 = $_POST['pass1'];
-		$pass2 = $_POST['pass2'];
-
-		if ($pass1 == $pass2) {
-		} else {
-	?>
-			<script>
-				alert("Las contraseñas ingresadas no coinciden");
-				location.href = "registro.php";
-			</script>
-	<?php
-
+    requiere("Conexiondb.php");
+    $checkemail=mysqli_query($mysqli,"SELECT * FROM usuarios WHERE email='$mail'");
+    $check_mail=mysqli_num_rows($checkemail);
+    if($pass==$rpass){
+        if($check_mail>0){
+            echo ' <script language="javascript">alert("Atencion, ya existe el mail designado para un usuario, verifique sus datos");</script> ';
+        }else{
+            mysqli_query($mysqli,"INSERT INTO usuarios VALUES('','$Nombre','$Apellido','$Correo','$Telefono','$Direccion','$Genero','$FechaNacimiento','$Genero','$FechaNacimiento','$Ciudad','$Nacionalidad','$pass)");
+				//echo 'Se ha registrado con exito';
+				echo ' <script language="javascript">alert("Usuario registrado con éxito");</script> ';
+				
+        }else{
+			echo 'Las contraseñas son incorrectas';
 		}
 
-		$SQL = "INSERT INTO USUARIO (NOMBRE, APELLIDO, CORREO, TELEFONO, DIRECCION, GENERO, FECHANACIMIENTO, CIUDAD, NACIONALIDAD, CONTRASENA, CONTRASENAREPETIDA) VALUES (:nombre, :ape, :correo, :tel, :direc, :genero,:fechanacim, :ciudad, : nacio, :pass1,:pass2)";
-		$Resultado = $conexion_db->prepare($SQL);
-		$Resultado->execute(array(":id" => 0, ":nom" => $Nombre, ":ape" => $Apellido, ":correo" => $Correo, ":tel" => $Telefono, ":direc" => $Direccion, ":genero" => $Genero, ":fechanacim" => $FechaNacimiento, ":ciudad" => $Ciudad, ":nacio" => $Nacionalidad, ":pass1" => $pass1, ":pass2" => $pass2));
-		header("Location:registro.php");
-	}
 
-	?>
-	<table align="center">
-		<tr>
-			<td colspan="5">
-				<FONT FACE="Delninoys" SIZE=10 COLOR="black">Market Place</FONT>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="4">
-				<h3>
-					Bienvenido al sitio web de <strong>MARKETPLACE</strong> en este puede realizar la reserva de diversos productos
-				</h3>
-			</td>
-			<td>
-				<a class="boton_login" href="../Registrousuario/index.php">Login</a>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<a class="button" href="../Home.php">Inicio</a>
-			</td>
-			<td>
-				<a class="button" href="../Catalogo/Catalogo.php">Catalogo</a>
-			</td>
-			<td>
-				<a class="button" href="../Reserva.php">Reserva</a>
-			</td>
-			<td>
-				<a class="button" href="registro.php">Inscripción</a>
-			</td>
-
-			<td>
-				<a class="button" href="../Contacto/Contacto.php"> Contactenos</a>
-			</td>
-
-		</tr>
-	</table>
-	<center>
-		<h1>FORMULARIO DE REGISTRO USUARIO</h1>
-	</center>
-	<Form action="<?php echo $_SERVER['PHP_SELF']; ?> " method="post">
-		<table width="50%" border="0" align="center">
-			<tr>
-				<td>Nombre: </td>
-				<td><input type='text' name='nom' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Apellido:</td>
-				<td><input type='text' name='ape' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Correo: </td>
-				<td><input type='email' name='correo' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Telefono: </td>
-				<td><input type='tel' name='tel' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Dirección: </td>
-				<td><input type='text' name='direc' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Genero: </td>
-				<!--<td><input type='text' name='genero' size='30' class='centrado' required></td>-->
-				<td><select name="genero" class='centrado' required>
-						<option selected value="0"> Elige una opción </option>
-						<option value="1">masculino</option>
-						<option value="2">femenino</option>
-					</select></td>
-			</tr>
-			<tr>
-				<td>Fecha Nacimiento: </td>
-				<!--<td><input type='text' name='fechanacim' size='30' class='centrado' required></td>-->
-				<td><input type='date' name='fechanacim' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Ciudad: </td>
-				<td><input type='text' name='ciudad' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Nacionalidad: </td>
-				<td><input type='text' name='nacio' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Contraseña: </td>
-				<td><input type='password' name='pass1' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td>Repite Contraseña: </td>
-				<td><input type='password' name='pass2' size='30' class='centrado' required></td>
-			</tr>
-			<tr>
-				<td class='bot' colspan="2">
-					<center><input class="button" type='submit' name='cr' id='cr' value='Enviar' required></center>
-				</td>
-
-			</tr>
-		</table>
-	</Form>
-</body>
-
-</html>
+?>
