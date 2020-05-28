fdccf316eee2c136ee277fddb8d9e32f1031d3b8
Keiichi243 <53916255+Keiichi243@users.noreply.github.com>
2020-05-28 18:05:38 -0500

    Add files via upload

---
+++ Reservas/Reserva.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang=»es-ES»>
+
+<head>
+
+<meta charset=’utf-8′>
+
+</head>
+
+<body>
+
+<form id=»buscador» name=»buscador» method=»post» action=»<?php echo $conexion_db['PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ'] ?>»>
+
+<input id=»buscar» name=»buscar» type=»search» placeholder=»Buscar aquí…» autofocus >
+
+<input type=»submit» name=»buscador» class=»boton peque aceptar» value=»buscar»>
+
+</form>
+
+<?php
+
+// Resultado, número de registros y contenido.
+
+echo Catalogo;
+
+?>
+
+</body>
+
+</html>
